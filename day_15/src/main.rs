727cfcc906a09b7b4e2385bc36db899176e48116
vicente410 <brunottvicente@gmail.com>
2024-05-07 17:57:54 +0100

    ran rustfmt

--- day_15/src/main.rs
+++ day_15/src/main.rs
@@ -1,12 +1,18 @@
-use std::fs;
 use std::cmp::max;
+use std::fs;
 
 fn main() {
     let input = fs::read_to_string("../input.txt").expect("Failed to read file");
     let ingredients = parse(input.trim());
-    
-    println!("Part 1: the best cookie scores {} points", find_cookie(&ingredients, 0));
-    println!("Part 2: with 500 calories it scores {} points", find_cookie(&ingredients, 500));
+
+    println!(
+        "Part 1: the best cookie scores {} points",
+        find_cookie(&ingredients, 0)
+    );
+    println!(
+        "Part 2: with 500 calories it scores {} points",
+        find_cookie(&ingredients, 500)
+    );
 }
 
 fn parse(input: &str) -> Vec<Vec<i32>> {
@@ -16,7 +22,7 @@ fn parse(input: &str) -> Vec<Vec<i32>> {
         let words: Vec<_> = line.split(' ').collect();
         let mut ing = Vec::new();
 
-        ing.push(words[2][..words[2].len() - 1].parse().unwrap()); 
+        ing.push(words[2][..words[2].len() - 1].parse().unwrap());
         ing.push(words[4][..words[4].len() - 1].parse().unwrap());
         ing.push(words[6][..words[6].len() - 1].parse().unwrap());
         ing.push(words[8][..words[8].len() - 1].parse().unwrap());
@@ -45,27 +51,26 @@ fn find_cookie(ingredients: &Vec<Vec<i32>>, target_calories: i32) -> i32 {
             }
         }
     }
-    
+
     best_score
 }
 
 fn calc_stats(ingredients: &Vec<Vec<i32>>, quantities: Vec<i32>) -> (i32, i32) {
-        let mut scores: Vec<i32> = vec![0; ingredients[0].len()];
-        let mut score = 1;
-        let calories;
+    let mut scores: Vec<i32> = vec![0; ingredients[0].len()];
+    let mut score = 1;
+    let calories;
 
-        for i in 0..ingredients.len() {
-            for j in 0..ingredients[0].len() {
-                scores[j] += ingredients[i][j] * quantities[i];
-            }
+    for i in 0..ingredients.len() {
+        for j in 0..ingredients[0].len() {
+            scores[j] += ingredients[i][j] * quantities[i];
         }
-
-        calories = scores.pop().unwrap();
-
-        for s in scores {
-            score *= max(s, 0);
-        }
-
-        (score, calories)
     }
 
+    calories = scores.pop().unwrap();
+
+    for s in scores {
+        score *= max(s, 0);
+    }
+
+    (score, calories)
+}
